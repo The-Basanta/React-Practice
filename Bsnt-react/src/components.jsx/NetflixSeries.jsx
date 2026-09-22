1cef022d9951aa4587674f28e5ff4e9ad072b836
The-Basanta <basanta.815424@ac.tu.edu.np>
2026-09-22 21:35:29 +0545

    trial

--- Bsnt-react/src/components.jsx/NetflixSeries.jsx
+++ Bsnt-react/src/components.jsx/NetflixSeries.jsx
@@ -12,11 +12,7 @@ const NetflixSeries = () => {
 };
 
 export const Footer = () => {
-  return (
-    <footer>
-      <p>© 2023 Netflix. All rights reserved.</p>
-    </footer>
-  );
+
 };
 
 export default NetflixSeries;
